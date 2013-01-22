9e9d5f717da27a6a742148a027b54bc512c518cc
Jens Krististian Villadsen <jenskristianvilladsen@gmail.com>
2013-01-22 23:15:50 +0100

    * Removed warnings

--- src/main/java/org/dyndns/jkiddo/protocol/dmap/chunks/unknown/Unknownml.java
+++ src/main/java/org/dyndns/jkiddo/protocol/dmap/chunks/unknown/Unknownml.java
@@ -1,19 +1,11 @@
 package org.dyndns.jkiddo.protocol.dmap.chunks.unknown;
 
 import org.dyndns.jkiddo.protocol.dmap.chunks.ContainerChunk;
-import org.dyndns.jkiddo.protocol.dmap.chunks.RawChunk;
-import org.dyndns.jkiddo.protocol.dmap.chunks.StringChunk;
 
 public class Unknownml extends ContainerChunk
 {
 	public Unknownml()
 	{
 		super("msml", "com.apple.itunes.unknown-ml");
-		// this(new byte[] {});
 	}
-
-	// public Unknownml(byte[] i)
-	// {
-	// super("msml", "com.apple.itunes.unknown-ml", i);
-	// }
 }
