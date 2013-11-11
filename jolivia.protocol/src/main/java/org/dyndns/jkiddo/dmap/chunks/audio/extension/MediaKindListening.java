e690a914820b8eb465ce7bc9049360dbcd87e0f4
jkiddo <jenskristianvilladsen@gmail.com>
2013-11-11 21:24:32 +0100

    * Added dunno

---
+++ jolivia.protocol/src/main/java/org/dyndns/jkiddo/dmap/chunks/audio/extension/MediaKindListening.java
@@ -0,0 +1,11 @@
+package org.dyndns.jkiddo.dmap.chunks.audio.extension;
+
+import org.dyndns.jkiddo.dmp.chunks.ContainerChunk;
+
+public class MediaKindListening extends ContainerChunk
+{
+	public MediaKindListening()
+	{
+		super("aeml", "com.apple.itunes.media-kind-listing");
+	}
+}
---
+++ jolivia.protocol/src/main/java/org/dyndns/jkiddo/dmap/chunks/audio/extension/MediaKindListeningItem.java
@@ -0,0 +1,11 @@
+package org.dyndns.jkiddo.dmap.chunks.audio.extension;
+
+import org.dyndns.jkiddo.dmp.chunks.ContainerChunk;
+
+public class MediaKindListeningItem extends ContainerChunk
+{
+	public MediaKindListeningItem()
+	{
+		super("aemi", "com.apple.itunes.media-kind-listing-item");
+	}
+}
